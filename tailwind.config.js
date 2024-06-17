0a79c991af0652f2c4202db81c761cb3ff835ab0
jiwon <jiwon590@eigene.ai>
2024-06-17 11:43:13 +0900

    style: css 초기화

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  corePlugins: {
+    preflight: true,
+  },
   theme: {
     extend: {},
   },
