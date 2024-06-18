a4cf86e6a114494ddf0447d9dce51245bf50a290
jiwon <jiwon590@eigene.ai>
2024-06-18 17:07:06 +0900

    feat: project 반응형

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -15,7 +15,7 @@ function Projects() {
           return (
             <div
               key={data.name}
-              className="w-[80%] h-[800px]  mb-10 mx-auto bg-white rounded-xl p-10 "
+              className="w-[80%] h-auto xl:h-[800px]  mb-10 mx-auto bg-white rounded-xl  p-10 "
             >
               <div className="flex flex-col items-center mb-10">
                 <p className="text-4xl">{data.name}</p>
@@ -25,30 +25,30 @@ function Projects() {
               </div>
 
               <div className="flex flex-col  w-full h-full  xl:flex-row">
-                <div className="h-[45%] xl:w-[50%] xl:h-[70%]">
+                <div className="w-[80%] h-[40%] mx-auto xl:w-[50%] xl:h-[70%]">
                   <img
                     className=" bg-cover object-fill w-full h-full brightness-100 group-hover:brightness-50"
                     src={`${data.img}`}
                   />
                 </div>
 
-                <div className=" mx-auto pt-5 xl:p-10 xl:w-[55%] xl:h-[80%] xl:ml-10 xl:border xl:border-slate-200 xl:rounded-xl ">
+                <div className="mx-auto pt-10 w-[70%]  xl:p-10 xl:w-[50%] xl:h-[80%] xl:ml-10xl:rounded-xl ">
                   <p className="pb-10 text-lg">{data.content}</p>
-
-                  <button
-                    onClick={() => window.open(data.githubUrl, "_blank")}
-                    className="flex flex-row justify-center items-center cursor-pointer  border border-spacing-1 rounded-lg p-1 mb-5 hover:text-gray-500"
-                  >
-                    <FaGithub size={20} className="mr-2" />
-                    <p className="text-lg">github 바로가기</p>
-                  </button>
-
-                  <div className="flex ">
+                  <div className=" border-b border-slate-200 ">
+                    <button
+                      onClick={() => window.open(data.githubUrl, "_blank")}
+                      className=" flex flex-row justify-center items-center cursor-pointer  border border-spacing-1 rounded-lg p-1 mb-5  hover:text-gray-500"
+                    >
+                      <FaGithub size={20} className="mr-2" />
+                      <p className="text-lg ">github 바로가기</p>
+                    </button>
+                  </div>
+                  <div className="flex mt-5 ">
                     <div className="flex  mr-8 min-w-[100px]">
                       <div className="mr-2 ">
                         <FaCheck />
                       </div>
-                      <span>Front-end</span>
+                      <span className="font-semibold">Front-end</span>
                     </div>
                     <div className="min-w-[350px] ">
                       {data.stack.map((stack) => {
@@ -63,7 +63,7 @@ function Projects() {
                         <div className="mr-2">
                           <FaCheck />
                         </div>
-                        <span>Back-end</span>
+                        <span className="font-semibold">Back-end</span>
                       </div>
                       <div className="min-w-[350px]">
                         {data?.back_stack?.map((stack) => {
@@ -78,7 +78,7 @@ function Projects() {
                       <div className="mr-2">
                         <FaCheck />
                       </div>
-                      <span>주요기능</span>
+                      <span className="font-semibold">주요기능</span>
                     </div>
                     <div>
                       {data.partsOfMe.map((description) => {
